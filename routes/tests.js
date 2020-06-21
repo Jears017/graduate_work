3b2dde550f211328275627b9d9cbe22ece21f2d2
Jears017 <turban01@mail.ru>
2020-06-21 18:44:09 +0300

    fix: modified UI

--- routes/tests.js
+++ routes/tests.js
@@ -4,48 +4,54 @@ const Tests = require('../models/test')
 const router = Router()
 
 router.get('/', async (req, res) => {
-    const tests = await Tests.find()
+    if (req.query.search) {
+        const regex = new RegExp(escapeRegex(req.query.search), 'gi')
+        const tests = await Tests.find({name: regex})
+        res.render('tests', {
+            title: 'Тесты',
+            isTest: true,
+            tests
+        })
+    } else {
+        const tests = await Tests.find()
+        res.render('tests', {
+            title: 'Тесты',
+            isTest: true,
+            tests
+        })
+    }
+})
 
-    res.render('tests', {
-        title: 'Тесты',
-        isTest: true,
-        tests
+router.get('/:id/edit', auth, async (req, res) => {
+    if (!req.query.allow) {
+        return res.redirect('/')
+    }
+
+    const test = await Tests.findById(req.params.id)
+    res.render('test-edit', {
+        title: `Редактировать ${test.title}`,
+        test
     })
 })
 
-    router.get('/:id/edit', auth, async (req, res) => {
-        if (!req.query.allow) {
-            return res.redirect('/')
-        }
+router.post('/edit', auth, async (req, res) => {
+    await Tests.findByIdAndUpdate(req.body.id, req.body)
+    res.redirect('/tests')
+})
 
-        const test = await Tests.findById(req.params.id)
-        res.render('test-edit', {
-            title: `Редактировать ${test.title}`,
-            test
-        })
-    })
+router.post('/remove', auth, async (req, res) => {
 
-    router.post('/edit', auth, async (req, res) => {
-        await Tests.findByIdAndUpdate(req.body.id, req.body)
+    try {
+        await Tests.deleteOne({ _id: req.body.id })
         res.redirect('/tests')
-    })
+    } catch (e) {
+        console.log(e)
+    }
 
-    router.post('/remove', auth, async (req, res) => {
+})
 
-        try {
-            await Tests.deleteOne({ _id: req.body.id })
-            res.redirect('/tests')
-        } catch (e) {
-            console.log(e)
-        }
-    
-    })
-   
-
-
-
-
-
-
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
 
 module.exports = router
